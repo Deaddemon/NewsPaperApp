16a99cedefd6f68fc4301a0cbd4243d64953b949
impetus007 <vishal2219singh@gmail.com>
2021-09-29 13:58:33 +0530

    updated

---
+++ static/js/signup.js
@@ -0,0 +1,13 @@
+var index = 0;
+var text = 'Glad To See  You  Here...';
+
+
+// Here you can put in the text you want to make it type.
+function type()
+{
+document.getElementById('screen').innerHTML +=text.charAt(index);
+
+index += 1;
+let t = setTimeout('type()',100);
+// The time taken for each character here is 100ms. You can change it if you want.
+}
