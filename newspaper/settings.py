83c878a61e3c0e0d0ee878d0217bb7695e8dcc94
Deaddemon <77224604+Deaddemon@users.noreply.github.com>
2021-09-29 19:31:30 +0530

    Update settings.py

--- newspaper/settings.py
+++ newspaper/settings.py
@@ -144,5 +144,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
-STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 django_heroku.settings(locals())
